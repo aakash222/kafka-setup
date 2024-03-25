03bbadc0529d75b71d511fa7b4e2c94524512957
aakash222 <aakashpatel2k18@gmail.com>
2024-03-25 12:49:34 +0530

    add independent kafka topic ingestion test

---
+++ kafkaIngestiontest/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	"github.com/linkedin/goavro/v2"
+	"github.com/segmentio/kafka-go"
+)
+
+func NewKafkaClient() *Kafka {
+	kafkaWriter := &kafka.Writer{
+		Addr:                   kafka.TCP("localhost:9092"),
+		AllowAutoTopicCreation: false,
+		RequiredAcks:           kafka.RequireOne,
+		MaxAttempts:            5,
+		BatchSize:              1,
+		Balancer:               kafka.Murmur2Balancer{},
+		//BatchTimeout: time.Millisecond * 10,
+	}
+	return &Kafka{
+		kafkaWriter: kafkaWriter,
+	}
+}
+
+type Kafka struct {
+	kafkaWriter *kafka.Writer
+}
+
+func (kf *Kafka) BatchPublish(messages []kafka.Message) error {
+	fmt.Println(messages)
+	return kf.kafkaWriter.WriteMessages(context.Background(), messages...)
+}
+
+func main() {
+	kafkaClient := NewKafkaClient()
+
+	TestFeatureInsert(kafkaClient, "live-chatroomjoineventsV2", "schema/sc-chatroomjoinevents.avsc")
+}
+
+func TestFeatureInsert(kafkaClient *Kafka, kafkaTopic string, schemaPath string) {
+	codec := getAvroCodecBySchemaPath(schemaPath)
+
+	chatroomIds := []string{"chatroomid1", "chatroomid2", "chatroomid3", "chatroomid4", "chatroomid5"}
+
+	for i, _ := range chatroomIds {
+		userid := "userid_1"
+		if i%2 == 0 {
+			userid = "userid_0"
+		}
+		d := fmt.Sprintf("counter _ %d", i)
+		fmt.Println(d)
+		row1 := map[string]interface{}{
+			"userId":     goavro.Union("string", userid),
+			"hostId":     goavro.Union("string", "hostId"),
+			"coHostId":   goavro.Union("string", "cohostId"),
+			"chatroomId": goavro.Union("string", chatroomIds[i]),
+			"time":       goavro.Union("long", time.Now().UnixMilli()),
+		}
+
+		binary, err := codec.BinaryFromNative(nil, row1)
+		if err != nil {
+			fmt.Println("error while converting native data to avro binary", map[string]interface{}{
+				"err":  err.Error(),
+				"data": row1,
+			})
+		}
+		err = kafkaClient.BatchPublish([]kafka.Message{
+			{
+				Topic: kafkaTopic,
+				Value: binary,
+			},
+		})
+		if err != nil {
+			fmt.Println("error while publishing message to kafka", map[string]interface{}{
+				"err":  err.Error(),
+				"data": row1,
+			})
+		}
+	}
+}
+
+
+
+
+func getAvroCodecBySchemaPath(filename string) *goavro.Codec {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("error in reading avro schema file", map[string]interface{}{
+			"err": err.Error(),
+		})
+		return nil
+	}
+	avroSchema := string(data)
+	avroCodec, err := goavro.NewCodecForStandardJSON(avroSchema)
+	if err != nil {
+		fmt.Println("error in loading the Avro schmea", map[string]interface{}{
+			"err":      err.Error(),
+			"fileName": filename,
+		})
+		return nil
+	}
+	return avroCodec
+}
